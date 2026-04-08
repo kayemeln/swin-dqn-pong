564b37c16555f32b48fcac0dc57d2bd309fb74d6
Nathaniel <nathaniel@im-me.com>
2026-04-08 13:53:58 +0100

    Altered main again, and states

--- states.py
+++ states.py
@@ -26,7 +26,6 @@ from gymnasium.wrappers import(
 img_size = (210, 160)  # original size of screen 
 n_frames = 4
 
-
 def modify_gym_env(env):
     """
     This function modifies the gymnasium environment to enable the preprocessing wrappers. 
@@ -42,25 +41,3 @@ def modify_gym_env(env):
     env = FrameStackObservation(env, n_frames)
     return env
 
-
-def make_evaluation_states(file, n_states):
-    """
-    This function draws n_states samples from a memory deque stored in file, 
-    and saves them as 'evaluation_states.pkl'. 
-    This batch can be used in training.py to keep track of the Q-values during training. 
-    """
-
-    # load the specified memory
-    D = torch.load(file)
-
-    # sample a minibatch from the memory and concatenate the states into a tensor
-    minibatch = random.sample(D, n_states)
-    states_batch = torch.cat(tuple(torch.tensor(d[0], dtype=torch.float32).unsqueeze(0) for d in minibatch))
-
-    # save the states
-    torch.save(states_batch, 'evaluation_states.pkl')
-
-
-if __name__ == '__main__':
-    # datasets are stored as 'data/data_{img_size}_{n_frames}.pkl'
-    make_evaluation_states('data/data_84x84_4.pkl', 128)
